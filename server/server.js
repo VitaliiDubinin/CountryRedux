82a60403dddd834ab7771345dff1e4557e7a19f4
Vitalii Dubinin <98089841+VitaliiDubinin@users.noreply.github.com>
2022-10-03 13:57:24 +0300

    heroku first commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const path = require("path");
 const express = require("express");
+const path = require("path");
 const app = express();
+const publicPath = path.join(__dirname, "..", "build");
 const port = process.env.PORT || 3200;
-const publicPath = path.join(__dirname, "..", "public");
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
